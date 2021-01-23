c1351fd919601adfd702dec37cc50c43afe6b378
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-01-23 16:19:41 +0530

    Update Discord_nitro.py

--- Discord_nitro.py
+++ Discord_nitro.py
@@ -4,17 +4,18 @@ from pynput import keyboard
 from playsound import playsound
 
 keyboardd = keyboard.Controller()
-no_of_links, i = 1028, 0
+no_of_links, i = 0, 0
 
 #  FUNC WHICH GENERATES LINK
 def NitroCode():
     code = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
     return f'https://discord.gift/{code}'
+
 #  CLOSE TAB FUNC
 def clik():
     keyboardd.press(keyboard.Key.ctrl)
     keyboardd.press("w")
-    time.sleep((.2))
+    time.sleep((.1))
     keyboardd.release(keyboard.Key.ctrl)
     keyboardd.release("w")
 
@@ -25,6 +26,7 @@ while not i == 1:
     webbrowser.open(Nitro)
     try:
         tim = time.time()
+        time.sleep(1)
         while not pyautogui.pixel(678, 316) == (79, 84, 92):
             """
             This is the pixel value of point of poo, 
