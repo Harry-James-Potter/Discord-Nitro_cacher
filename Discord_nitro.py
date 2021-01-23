313f259ab9860a32c263cfdf00fa7e625c9f1621
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-01-23 15:53:26 +0530

    Update Discord_nitro.py

--- Discord_nitro.py
+++ Discord_nitro.py
@@ -1,26 +1,36 @@
-# CURRENTLY WORKING
 import random, string, time, webbrowser, pyautogui
 from pynput import keyboard
+from playsound import playsound
 keyboardd = keyboard.Controller()
 no_of_links = 0
-while True:
+i = 0
+while not i == 1:
     def NitroCode():
         code = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
         return f'https://discord.gift/{code}'
+    def clik():
+        keyboardd.press(keyboard.Key.ctrl)
+        keyboardd.press("w")
+        time.sleep((1 / 3))
+        keyboardd.release(keyboard.Key.ctrl)
+        keyboardd.release("w")
     no_of_links += 1
     print(f"{no_of_links}) {NitroCode()}")
     webbrowser.open(NitroCode())
     try:
+        tim = time.time()
         while not pyautogui.pixel(678, 316) == (79, 84, 92):
             """
             This is the pixel value of point of poo, 
             x and y coordinates might be different on yours
             """
+            if time.time() - tim >= 30:
+                i = 1
+                break
             continue
     except WindowsError:
         continue
-    keyboardd.press(keyboard.Key.ctrl)
-    keyboardd.press("w")
-    time.sleep(0.25)
-    keyboardd.release(keyboard.Key.ctrl)
-    keyboardd.release("w")
+    clik()
+
+#  ALARM / ANY MUSIC AFTER THE RIGHT NITRO IS CREATED
+playsound("")
