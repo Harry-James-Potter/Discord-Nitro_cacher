3b232d74345249be0c1b3101d5a7391e8c138f41
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-01-23 16:13:41 +0530

    Update Discord_nitro.py

--- Discord_nitro.py
+++ Discord_nitro.py
@@ -1,18 +1,24 @@
+#  IMPORTING MOSDLES
 import random, string, time, webbrowser, pyautogui
 from pynput import keyboard
 from playsound import playsound
+
 keyboardd = keyboard.Controller()
 no_of_links, i = 1028, 0
+
+#  FUNC WHICH GENERATES LINK
+def NitroCode():
+    code = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    return f'https://discord.gift/{code}'
+#  CLOSE TAB FUNC
+def clik():
+    keyboardd.press(keyboard.Key.ctrl)
+    keyboardd.press("w")
+    time.sleep((.2))
+    keyboardd.release(keyboard.Key.ctrl)
+    keyboardd.release("w")
+
 while not i == 1:
-    def NitroCode():
-        code = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-        return f'https://discord.gift/{code}'
-    def clik():
-        keyboardd.press(keyboard.Key.ctrl)
-        keyboardd.press("w")
-        time.sleep((.2))
-        keyboardd.release(keyboard.Key.ctrl)
-        keyboardd.release("w")
     no_of_links += 1
     Nitro = NitroCode()
     print(f"{no_of_links}) {Nitro}")
